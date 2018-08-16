6c841aa5124ac46129c121e82e853d534a8d5bab
AreannaWhittington <whittingtonare@ucityschools.org>
2018-08-15 22:23:37 -0500

    almost done

--- ws/dist/style.6b3c5b2c.js
+++ ws/dist/style.6b3c5b2c.js
@@ -169,7 +169,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./28-5-18-5553.jpg":68,"_css_loader":6}],17:[function(require,module,exports) {
+},{"./28-5-18-5553.jpg":68,"_css_loader":6}],22:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '62056' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54915' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -418,4 +418,4 @@ LazyPromise.prototype.catch = function (onError) {
 };
 },{"./bundle-url":7}],0:[function(require,module,exports) {
 var b=require(63);b.load([["28-5-18-5553.fd37b106.jpg",68]]);
-},{}]},{},[17,0], null)
+},{}]},{},[22,0], null)
